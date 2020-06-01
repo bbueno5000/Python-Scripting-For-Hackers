b5d8f9e5a624270fd0f84548c74b7305b9f68a6c
B. Bueno <bbueno25@hotmail.com>
2020-06-01 11:07:56 -0600

    Python Scripting for Hackers, Part 2: Building a Banner Grabbing Tool

--- python_scripting_for_hackers/python_scripting_for_hackers.py
+++ python_scripting_for_hackers/python_scripting_for_hackers.py
@@ -1,1 +1,28 @@
+"""
+DOCSTRING
+"""
+import socket
 
+def network_connection():
+    """
+    DOCSTRING
+    """
+    s = socket.socket()
+    s.connect('192.168.1.109', 22)
+    answer = s.recv(1024)
+    print(answer)
+    s.close
+
+def print_variables():
+    """
+    DOCSTRING
+    """
+    string_variable = 'Hackers-Arise is the best place to learn hacking.'
+    integer_variable = 12
+    floating_point_variable = 3.1415
+    list_variable = [1, 2, 3, 4, 5]
+    dictionary_variable = {'name':'OccupyTheWeb', 'value':27}
+    print(string_variable)
+    print(integer_variable)
+    print(floating_point_variable)
+    print(list_variable[3])
